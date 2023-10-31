52cb189d294fd14b3e8b10aa4bd78dc4205be897
Markus <markus@s7d.io>
2023-10-31 01:28:33 +0100

    Initialized Webapp Project

---
+++ minerva-webapp/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../styles/global.css';
+</script>
+
+<svelte:head>
+	<title>Minerva</title>
+	<link rel="icon" href="/favicon.svg" />
+</svelte:head>
+
+<main>
+	<slot />
+</main>
---
+++ minerva-webapp/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to <span>Minerva</span></h1>
+<p>Soon there will be content here...</p>
